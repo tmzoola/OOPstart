7d3d5df22baa442936d68119cb0f62b68d7448c4
tmzoola <murodtokh1997@gmail.com>
2020-04-26 14:34:29 +0300

    Initial Commit

--- classesfirst.py
+++ classesfirst.py
@@ -1,3 +1,25 @@
-print("hello World")
+class Computer:
 
-print("Welcome to OOP")
+    def __init__(self):
+        self.name = "Murodjon"
+        self.age = 28
+    def update(self):
+        self.age = 10
+    def compare(self,other):
+        if self.age == other.age:
+            return True
+        else:
+            return False
+
+
+c1 = Computer()
+c1.update()
+c2 = Computer()
+
+if c1.compare(c2):
+    print("they are same")
+else:
+    print("They are different")
+
+print(c1.age)
+print(c2.age)
